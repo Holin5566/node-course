93dc4db83e4947a592f5851312119c62d5f10f79
chl5566 <tukuchl5566@gmail.com>
2022-05-08 14:27:05 +0800

    first

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log("hello");
+function sum(n) {
+  return ((n + 1) * n) / 2;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100)); // 5050
