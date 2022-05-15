6941333b2b2cfdaf84781a3850dd6e08cd9fc12e
chl5566 <tukuchl5566@gmail.com>
2022-05-15 11:36:27 +0800

    add 可填入檔名的readfile

--- basic.js/readFile.js
+++ basic.js/readFile.js
@@ -12,4 +12,20 @@ const read = new Promise((resovle, reject) => {
   });
 });
 
-read.then((txt) => console.log(txt)).catch((err) => console.log(err));
+// read.then((txt) => console.log(txt)).catch((err) => console.log(err));
+
+function readWhat(fileName) {
+  return new Promise((resovle, reject) => {
+    rf.readFile(fileName, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resovle(data);
+      }
+    });
+  });
+}
+
+readWhat("text.txt")
+  .then((txt) => console.log(txt))
+  .catch((err) => console.log(err));
