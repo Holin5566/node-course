75b55f951576cab69ef0f7d3a81f08aeb3474198
chl5566 <tukuchl5566@gmail.com>
2022-05-21 16:23:49 +0800

    add server.js

--- simple-express/server.js
+++ simple-express/server.js
@@ -5,14 +5,7 @@ const app = express();
 require("dotenv").config();
 const env = process.env;
 
-const connection = mysql.createConnection({
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-});
-console.log("connect to MySQL");
+connection();
 
 app.get("/", (req, res) => {
   res.send("hello world!");
@@ -21,3 +14,14 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+async function connection() {
+  await mysql.createConnection({
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+  });
+  console.log("connect to MySQL");
+}
