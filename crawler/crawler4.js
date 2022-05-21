f62ec81b3a58505bdab24f61d3ddbd9d77ab50c2
chl5566 <tukuchl5566@gmail.com>
2022-05-21 11:41:28 +0800

    add .env

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const env = process.env;
 
 (async () => {
   const connection = await mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "user",
-    password: "user123",
+    password: env.PASSWORD,
     database: "stock",
   });
   let [data, result] = await connection.execute("SELECT * FROM `stocks`");
